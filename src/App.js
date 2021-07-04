82afd6986f8bd5fb6e98609c84567cf3f3b6a195
shivamnarkar47 <shivamnarkar47@gmail.com>
2021-07-04 18:38:28 +0530

    2 files modified, 6 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Header from './Header';
+import Body from './Body';
+import Music from './Music';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="app">
+      <Header className="main_header" />
+      <Body className="main_body" />
+      <Music className="main_music" />
     </div>
   );
 }
---
+++ src/Music.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Music.css';
+
+function Music() {
+  return (
+    <div className="music">
+      <h1>I am Music</h1>
+    </div>
+  );
+}
+
+export default Music;
